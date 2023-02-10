67a1dfc65e2ca2574fa77b6d2bb0011b9af9ee88
job_family_new <jayesh.bhosale@neosoftmail.com>
2023-02-10 15:59:15 +0530

    adding page

--- src/app/calculator-section/calculator-section.component.ts
+++ src/app/calculator-section/calculator-section.component.ts
@@ -6,12 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator-section.component.scss']
 })
 export class CalculatorSectionComponent {
-  loan_value=1000000;
-
-  /* progressbar */
+  progress = '40%';
   data:any;
-  progress: any;
+
   private updateSliderToggle: boolean = false;
+
+  percentage = 40;
+  loanvalue = this.percentage * 25000;
+
+  loanselect_val=false;
+  loan_li= [{imgurl:'../../assets/images/icons/home-icon.svg', options:'Home Loan'},
+            {imgurl:'../../assets/images/icons/home-icon.svg', options:'Personal Loan'}
+          ];
+
 
   startUpdateSlider(data:any) {
     this.updateSliderToggle = true;
@@ -23,16 +30,64 @@ export class CalculatorSectionComponent {
 
   updateSlider(event:any, data:any) {
     if (this.updateSliderToggle) {
-      let percentage: number = Math.floor(
+      this.loanvalue = 0;
+      this.percentage = Math.floor(
         (event.layerX / (event.target.offsetWidth - 3)) * 100
       );
 
-      if (percentage > 100) {
-        percentage = 100;
-      } else if (percentage < 0) {
-        percentage = 0;
+      if (this.percentage > 100) {
+        this.percentage = 100;
+      } 
+      else if (this.percentage <= 0){
+        this.percentage = 0;
       }
-      this.progress = percentage + '%';
+      this.progress = this.percentage + '%';
+      this.loanvalue = this.percentage * 25000;
+      console.log(this.loanvalue);
     }
+  }
+
+  decreaseLoan(){
+    if(this.loanvalue <= 200000){
+      this.loanvalue= 200000;
+      this.percentage= 0;
+      this.progress = this.percentage + '%'; 
+    }
+    else{
+      this.loanvalue= this.loanvalue - 100000;
+      this.percentage= (this.loanvalue * 100)/2500000;
+      this.progress = this.percentage + '%'; 
+    }
+  }
+  increaseLoan(){
+    if(this.loanvalue >= 2500000){
+      this.loanvalue= 2500000;
+      this.percentage= (this.loanvalue * 100)/2500000;
+      this.progress = this.percentage + '%'; 
+    }
+    else{
+      this.loanvalue= this.loanvalue + 100000;
+      this.percentage= (this.loanvalue * 100)/2500000;
+      this.progress = this.percentage + '%'; 
+    }
+  }
+
+  /* ul show and hide functionality */
+  imgpath='../../assets/images/icons/home-icon.svg';
+  selectedfield='Home Loan';
+  
+  ulshowhide(){
+    if(this.loanselect_val==true){
+      this.loanselect_val=false;
+    }
+    else{
+      this.loanselect_val=true;
+    }
+  }
+
+  clicked(index:any){
+    console.log(this.loan_li[index]);
+    this.imgpath=this.loan_li[index].imgurl;
+    this.selectedfield= this.loan_li[index].options;
   }
 }
