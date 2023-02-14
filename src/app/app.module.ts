7f521ad86d3e9c01fb8ce86c4ef3eeb9defc9e42
job_family_new <jayesh.bhosale@neosoftmail.com>
2023-02-14 18:54:41 +0530

    adding custoner support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -34,9 +37,12 @@ import { CustomerSecComponent } from './customer-sec/customer-sec.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
--- src/app/customer-sec/customer-sec.component.ts
+++ src/app/customer-sec/customer-sec.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-customer-sec',
   templateUrl: './customer-sec.component.html',
-  styleUrls: ['./customer-sec.component.scss']
+  styleUrls: ['./customer-sec.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CustomerSecComponent {
-
   
 }
